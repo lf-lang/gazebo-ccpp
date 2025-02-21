eda59391b37d36bc0bf292b64bf0c637eada5b6b
erlingrj <erling.jellum@gmail.com>
2025-02-21 10:14:15 +0100

    Add convenience function for subscribing to gazebo topic

--- src/include/util.hpp
+++ src/include/util.hpp
@@ -1,5 +1,18 @@
 #pragma once
 
+#include <memory>
+#include <functional>
+template <typename MsgType>
+void generic_callback(void* lf_handle, const MsgType& msg) {
+    auto msg_copy = new MsgType(msg);
+		instant_t stamp = SEC(msg.header().stamp().sec()) + NSEC(msg.header().stamp().nsec());
+		interval_t delay = stamp - lf_time_physical();
+		if (delay < 0) {
+			delay = 0;
+		}
+    lf_schedule_value(lf_handle, delay, static_cast<void*>(msg_copy), 1);
+}
+
 // This templated function can be passed to lf_set_destructor so that
 // the C-runtimes token management uses `delete` instead of the default `free`.
 // This is necessary if the tokens were created with `new`.
@@ -7,3 +20,4 @@ template<typename T>
 void cpp_delete(void * value) {
 		delete static_cast<T*>(value);
 }
+
